3fd60eb78d7723ec139a6c340ac60f81c5e21e32
Filipe <f.melo@marquesconsult.com.br>
2020-11-25 10:47:52 -0300

    Build refinery

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
+from sc2.constants import UnitTypeId
+
 class WorkerRushBot(sc2.BotAI):
     async def on_step(self, iteration: int):
-        if iteration == 0:
-            for worker in self.workers:
-                worker.attack(self.enemy_start_locations[0])
+        await self.distribute_workers()
+        await self.build_refineries()
+
+    async def build_refineries(self):
+        # If we have no refinery, build refinery
+        if (
+            self.gas_buildings.amount + self.already_pending(UnitTypeId.REFINERY) == 0
+            and self.can_afford(UnitTypeId.REFINERY)
+            and self.workers
+        ):
+            scv: Unit = self.workers.random
+            target: Unit = self.vespene_geyser.closest_to(scv)
+            scv.build_gas(target)
+
 
 run_game(maps.get("AcropolisLE"), [
     Bot(Race.Terran, WorkerRushBot()),
